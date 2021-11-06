417affcd018662c522770cd7f41cdbb8f2a78c8d
ntokozo <ntokozo.majozi@global.ntt>
2021-11-06 10:53:37 +0200

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MobileComponent } from './mobile/mobile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mobile', component: MobileComponent },
+  { path: '', redirectTo: 'mobile', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileComponent } from './mobile/mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileComponent
   ],
   imports: [
     BrowserModule,
